36b6d0f404691268c4301e38d21eca1b4579a6e4
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2020-01-06 20:11:01 -0700

    Makes color wheel.

---
+++ source/whell.js
@@ -0,0 +1,7 @@
+function makeWheel(x, y) {
+  // If it is not the right data type, we halt the function by returning -1.
+  if(typeof(x) != 'number' || typeof(y) != 'number') {
+    return -1;
+  }
+  // Will add later.
+}
